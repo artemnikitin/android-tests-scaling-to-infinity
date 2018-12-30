08706a35b3a8425741dcd4dfcaf2769d0ca0bf73
Artem Nikitin <artemnikitin@artemnikitin.com>
2018-12-30 13:42:05 +0100

    Fix to make it work out of the box in the blog post example

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd /tool
-./android_emulator_waiter.py -e my_emulator_name
+./android_emulator_waiter.py -e android-emulator
 
 cd /app
 ./gradlew clean testDebugUnitTest connectedAndroidTest
